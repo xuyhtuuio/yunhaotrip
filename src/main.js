1ab4434758c174bfcd00f49af44242b953f4b328
cuiyunhao <cyl5912@163.com>
2023-03-24 17:35:16 +0800

    Revert "第一次提交"
    This reverts commit 3667971

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import "normalize.css"
-import "./assets/css/index.css"
-import pinia from "./store/index.js";
-import router from "./router/index.js";
-
-createApp(App).use(router).use(pinia).mount('#app')
